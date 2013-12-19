78cc48f34e8c17b75e859869d487bc6e5eeee976
marcusmolchany <marcusmolchany@gmail.com>
2013-12-18 21:12:30 -0500

    added about folder and removed Contact tab

--- index.php
+++ index.php
@@ -39,7 +39,6 @@
             <li class="active"><a href="#">Home</a></li>
             <li><a href="blog">Blog</a></li>
             <li><a href="about">About</a></li>
-            <li><a href="contact">Contact</a></li>
           </ul>
         </div><!-- /.nav-collapse -->
       </div><!-- /.container -->
