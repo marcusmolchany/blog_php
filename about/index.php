7864b0df6fad878ca21eeee5bd95efebe102ce94
marcusmolchany <marcusmolchany@gmail.com>
2013-12-20 22:30:15 -0500

    fixed document root issue

--- about/index.php
+++ about/index.php
@@ -5,16 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="shortcut icon" href="/first_twitter_bootstrap_page/images/favicon.png">
+    <link rel="shortcut icon" href="<?php echo $_SERVER['DOC_ROOT']; ?>/images/favicon.png">
 
     <title>Marcus Molchany's Blog (Build on Bootstrap)</title>
 
     <!-- Bootstrap core CSS -->
-    <link rel="stylesheet" type ="text/css" href="/first_twitter_bootstrap_page/dist/css/bootstrap.min.css"/>
+    <link rel="stylesheet" type ="text/css" href="<?php echo $_SERVER['DOC_ROOT']; ?>/dist/css/bootstrap.min.css"/>
 
     <!-- Custom styles for this template -->
-    <link href="/first_twitter_bootstrap_page/css/offcanvas.css" rel="stylesheet">
-    <link href="/first_twitter_bootstrap_page/css/custom.css"    rel="stylesheet">
+    <link href="<?php echo $_SERVER['DOC_ROOT']; ?>/css/offcanvas.css" rel="stylesheet">
+    <link href="<?php echo $_SERVER['DOC_ROOT']; ?>/css/custom.css"    rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
@@ -32,15 +32,15 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="/first_twitter_bootstrap_page/">Marcus Molchany's Blog</a>
+          <a class="navbar-brand" href="<?php echo $_SERVER['DOC_ROOT']; ?>/">Marcus Molchany's Blog</a>
         </div>
         <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav">
-            <li><a href="/first_twitter_bootstrap_page/">Home</a></li>
-            <li><a href="/first_twitter_bootstrap_page/blog">Blog</a></li>
+            <li><a href="<?php echo $_SERVER['DOC_ROOT']; ?>/">Home</a></li>
+            <li><a href="<?php echo $_SERVER['DOC_ROOT']; ?>/blog">Blog</a></li>
           </ul>
           <ul class="nav navbar-nav navbar-right">
-            <li class="active"><a href="/first_twitter_bootstrap_page/about">About</a></li>
+            <li class="active"><a href="<?php echo $_SERVER['DOC_ROOT']; ?>/about">About</a></li>
           </ul>
         </div><!-- /.nav-collapse -->
       </div><!-- /.container -->
@@ -54,7 +54,7 @@
             <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
           </p>
           <div class="jumbotron">
-            <img src="/first_twitter_bootstrap_page/images/profile.jpg" class="img-circle img-responsive" id="profile">
+            <img src="<?php echo $_SERVER['DOC_ROOT']; ?>/images/profile.jpg" class="img-circle img-responsive" id="profile">
             <h1>About</h1>
             <p>Marcus Molchany</p>
             <a href="https://twitter.com/molchanimal" target="_blank">@molchanimal</a>
@@ -63,7 +63,7 @@
             <p></p>
             <a href="https://linkedin.com/in/marcusmolchany" target="_blank">linkedin</a>
             <p></p>
-            <a href="/first_twitter_bootstrap_page/images/Resume_2013.pdf" target="_blank">resume</a>
+            <a href="<?php echo $_SERVER['DOC_ROOT']; ?>/images/Resume_2013.pdf" target="_blank">resume</a>
           </div>
         </div><!--/span-->
 
@@ -92,8 +92,8 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="/first_twitter_bootstrap_page/assets/js/jquery.js"></script>
-    <script src="/first_twitter_bootstrap_page/dist/js/bootstrap.min.js"></script>
-    <script src="/first_twitter_bootstrap_page/js/offcanvas.js"></script>
+    <script src="<?php echo $_SERVER['DOC_ROOT']; ?>/assets/js/jquery.js"></script>
+    <script src="<?php echo $_SERVER['DOC_ROOT']; ?>/dist/js/bootstrap.min.js"></script>
+    <script src="<?php echo $_SERVER['DOC_ROOT']; ?>/js/offcanvas.js"></script>
   </body>
 </html>
