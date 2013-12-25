1f03281cf3572011f455ec0ba858e9c8594f0beb
marcusmolchany <marcusmolchany@gmail.com>
2013-12-24 22:06:49 -0500

    added pager to blog

--- blog/index.php
+++ blog/index.php
@@ -100,6 +100,13 @@
         </div><!--/span-->
       </div><!--/row-->
 
+      <div class="" id="blogpager" role="navigation">
+        <ul class="pager">
+          <li class="previous"><a href="#">&larr; Older</a></li>
+          <li class="next"><a href="#">Newer &rarr;</a></li>
+        </ul>
+      <div>
+
       <?php
         $footer = new Footer();
 
