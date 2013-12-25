fdea7ed109b06aac96c028c76ed2b1230916efb7
marcusmolchany <marcusmolchany@gmail.com>
2013-12-24 22:00:05 -0500

    Changed title in NavBar

--- lib/NavBar.php
+++ lib/NavBar.php
@@ -17,7 +17,7 @@ class NavBar {
 		            <span class='icon-bar'></span>
 		            <span class='icon-bar'></span>
 		          </button>
-		          <a class='navbar-brand' href='" . $_SERVER['DOC_ROOT'] . "/'>Marcus Molchany's Blog</a>
+		          <a class='navbar-brand' href='" . $_SERVER['DOC_ROOT'] . "/'>Marcus Molchany</a>
 		        </div>
 		        <div class='collapse navbar-collapse'>
 		          <ul class='nav navbar-nav'>";
