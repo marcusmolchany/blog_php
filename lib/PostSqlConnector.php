6496a64d58b367cfbc0cb728fbc507c90b30b251
marcusmolchany <marcusmolchany@gmail.com>
2014-01-09 23:04:59 -0500

    removed db and table names from PostSqlConnector constructor and moved them to .htaccess

--- lib/PostSqlConnector.php
+++ lib/PostSqlConnector.php
@@ -5,7 +5,8 @@ include $_SERVER['DOC_ROOT'] . "SqlConnector.php";
 class PostSqlConnector extends SqlConnector{
 
 	public function __construct($hostName, $userName, $password) {
-		parent::__construct($hostName, $userName, $password, 'marcusdb', 'post');
+		//parent::__construct($hostName, $userName, $password, 'marcusdb', 'post');
+		parent::__construct($hostName, $userName, $password, $_SERVER['DB_NAME'], $_SERVER['TABLE_NAME']);
 	}
 
 	public function getAssociativityArray() {
