244b97b19cca7e48b8ddd518891c4f621dcd361f
marcusmolchany <marcusmolchany@gmail.com>
2014-01-12 22:08:45 -0500

    added .htaccess and modified PostSqlConnector.php to read in variables from config.ini

--- src/common/PostSqlConnector.php
+++ src/common/PostSqlConnector.php
@@ -6,7 +6,9 @@ class PostSqlConnector extends SqlConnector{
 
 	public function __construct($hostName, $userName, $password) {
 		//parent::__construct($hostName, $userName, $password, 'marcusdb', 'post');
-		parent::__construct($hostName, $userName, $password, $_SERVER['DB_NAME'], $_SERVER['TABLE_NAME']);
+		$config = parse_ini_file($_SERVER['DOC_ROOT'] . "../config.ini");
+		
+		parent::__construct($hostName, $userName, $password, $config['db_name'], $config['table_name']);
 	}
 
 	public function getAssociativityArray() {
